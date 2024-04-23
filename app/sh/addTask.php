8e56647bcccd837771468132a4dad67a6330bcf1
jsa <jsa@infraspeak.com>
2024-04-23 10:00:05 +0100

    finished

--- app/sh/addTask.php
+++ app/sh/addTask.php
@@ -3,6 +3,16 @@ namespace App;
 require '../../vendor/autoload.php';
 
 $taskName = readline("Enter task name: ");
+echo ("Suggestion task description:\n");
+
+$context = stream_context_create(array(
+    'http' => array(
+        'timeout' => 36000      // Timeout in seconds
+    )
+));
+
+$response = file_get_contents('http://127.0.0.1:8000/task_description/'.$taskName, false, $context);
+echo $response;
 $taskDescription = readline("Enter task description: ");
 
 $task = new Task();
